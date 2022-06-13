d1b618ea87d0b8bfd6f1d5ef01179591f06e5802
sabitig <sabitov0101@gmail.com>
2022-06-13 17:26:35 +0400

    transition between pages implemented

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert 'login' in Login_Page, "there is no login substring in the current url"
+        assert 'login' in self.browser.current_url, "there is no login substring in the current url"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form field not present"
